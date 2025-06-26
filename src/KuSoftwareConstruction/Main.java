826b42c8840c53bd06d675fcd5a24d0f1bd8d54a
DucKyWay <niji2549@gmail.com>
2025-06-26 10:06:24 +0700

    test

---
+++ src/KuSoftwareConstruction/Main.java
@@ -0,0 +1,14 @@
+package KuSoftwareConstruction;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		int x = 5;
+		if(x > 3) {
+			System.out.println("x is greater than 3");
+		}
+		
+	}
+
+}
