dad807f06125b39d7e14044426fbbde3ecd6d130
DucKyWay <niji2549@gmail.com>
2025-07-08 23:32:54 +0700

    exercise 2

--- src/com/suphawinee/ku/KuSoftwareConstruction/KuSoftwareConstructionMain.java
+++ src/com/suphawinee/ku/KuSoftwareConstruction/KuSoftwareConstructionMain.java
@@ -1,16 +1,35 @@
-package com.suphawinee.ku.KuSoftwareConstruction;
+package src.com.suphawinee.ku.KuSoftwareConstruction;
+
+import java.util.Scanner;
 
 public class KuSoftwareConstructionMain {
 
 	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
 		
 		System.out.println("Welcome");
 		
-		int x = 5;
-		if(x > 3) {
-			System.out.println("x is greater than 3");
-		}
+		int i = 0;
+		double d = i + 0 + 1L;
+		System.out.print(d);
 		
+		System.out.println();
 	}
 
 }
+
+/*
+จงเขียนโปรแกรมสำหรับกำรตัดเกรด โดยรับ input จำก user เป็นคะแนนรวม % (ทศนิยม 0-100)และตัดเกรดอิงเกณฑ์และมีเงื่อนไขเพิ่มเติมดังปรำกฏในตำรำงด้ำนล่ำง
+*ทั้งนี้กำรออกเกรดจะมีข้อพิจำรณำเพิ่มเติมดังนี้
+1. ขำดเรียนไม่เกิน 3 ครั้ง หำกขำดเกินได้เกรด F
+2. ต้องมีคะแนนสอบรวมกลำงภำคและปลำยภำคอย่ำงต่ำ 40 %หำกคะแนนสอบรวมไม่ถึง 40 %จะได้เกรด Fแม้ว่ำคะแนนรวมทั้งหมดจะ >=50%ก็ตำม
+3. สถำนะของกำรทำโปรเจคต้องเสร็จสมบูรณ์หำกโปรเจคไม่สมบูรณ์ จะได้เกรด I
+
+คะแนนรวม:
+- คะแนนรวม (100)
+- คะแนนสอบกลาง + ปลายภาค (35)
+- จานวนครั้งขาดเรียน
+- สถานการณ์ทาโปรเจค(true/false)
+
+ */
---
+++ src/com/suphawinee/ku/KuSoftwareConstruction/TypeInJava_2/Nanosecond.java
@@ -0,0 +1,13 @@
+package src.com.suphawinee.ku.KuSoftwareConstruction.TypeInJava_2;
+
+public class Nanosecond {
+    public static long minToNanoSec(int min){
+        long nanoSec = (long) min *60*1_000_000_000;
+        return (nanoSec);
+    }
+    public static void main(String[] args) {
+        System.out.println(minToNanoSec(0));
+        System.out.println(minToNanoSec(1));
+        System.out.println(minToNanoSec(10));
+    }
+}
